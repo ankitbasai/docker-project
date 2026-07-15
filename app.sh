df7485f1a818809f268574acd2b03f6dd28921f8
ankitbasai <ankitrajputbasai@gmail.com>
2026-07-15 18:26:17 +0530

    Update app.sh

--- app.sh
+++ app.sh
@@ -3,5 +3,4 @@ sudo apt-get update
 sudo apt-get install apache2 -y
 sudo systemctl start apache2
 sudo systemctl enable apache2
- cd /var/www/html
- echo "hyy" > index.html
+echo "hyy" | sudo tee /var/www/html/index.html
